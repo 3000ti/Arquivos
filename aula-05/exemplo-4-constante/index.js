23712c94b8ad14991feb1784c4b5da774e5c00ee
3000ti <166620549+3000ti@users.noreply.github.com>
2024-04-16 20:10:18 -0300

    [update] Comentários no exemplo 4

--- aula-05/exemplo-4-constante/index.js
+++ aula-05/exemplo-4-constante/index.js
@@ -1,8 +1,12 @@
 
+// Criar uma constante
 const idade = 10
 
+// Imprimir valor no console
 console.log(idade)
 
+// Alterar valor na constante
 idade = 11
 
+// Imprimir novamente
 console.log(idade)
