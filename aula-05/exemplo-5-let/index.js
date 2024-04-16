407dc15ea85187670f871ed63d53e913525c5923
3000ti <166620549+3000ti@users.noreply.github.com>
2024-04-16 20:11:15 -0300

    [update] Comentários no exemplo 5

--- aula-05/exemplo-5-let/index.js
+++ aula-05/exemplo-5-let/index.js
@@ -1,8 +1,12 @@
 
+// Criar uma variável
 let idade = 10
 
+// Imprimir valor no console
 console.log(idade)
 
+// Alterar valor da variável
 idade = 25
 
+// Imprimir valor novamente
 console.log(idade)
