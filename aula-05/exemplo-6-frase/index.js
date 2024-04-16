f8e03e717b964ae1872a73b0198bea76a15c561f
3000ti <166620549+3000ti@users.noreply.github.com>
2024-04-16 20:12:10 -0300

    [update] Comentários no exemplo 6

--- aula-05/exemplo-6-frase/index.js
+++ aula-05/exemplo-6-frase/index.js
@@ -1,5 +1,7 @@
 
+// Criar variáveis com nome e idade
 const nome = "José"
 const idade = 88
 
+// Imprimir frase no console concatenando strings e variáveis
 console.log("Olá! Meu nome é", nome, "e eu tenho", idade, "anos")
