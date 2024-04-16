3907da75056a273625a3a530daf0f0b89a1776b8
3000ti <166620549+3000ti@users.noreply.github.com>
2024-04-16 20:07:14 -0300

    [update] Comentários no exemplo 10

--- aula-05/exemplo-10-prompt/index.js
+++ aula-05/exemplo-10-prompt/index.js
@@ -1,8 +1,12 @@
 
+// Perguntar nome e idade ao usuário
 const nome = prompt("Qual é o seu nome?")
 const idade = prompt("Qual é a sua idade?")
+
+// Converter a idade para numeral
 const idadeNumero = Number(idade)
 
+// Imprimir variáveis e os seus tipos no console
 console.log("Nome do usuário:", nome)
 console.log("Tipo da constante nome:", typeof nome)
 
