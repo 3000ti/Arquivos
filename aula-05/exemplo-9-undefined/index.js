8d58bfebf21d9edb25cdb8a493514f4bd236eb30
3000ti <166620549+3000ti@users.noreply.github.com>
2024-04-16 20:14:33 -0300

    [update] Comentários no exemplo 9

--- aula-05/exemplo-9-undefined/index.js
+++ aula-05/exemplo-9-undefined/index.js
@@ -1,15 +1,21 @@
 
+// Declarar uma nova variável
 let idade
 
+// Imprimir valor e tipo no console
 console.log("Valor da idade:", idade)
 console.log("Tipo da idade:", typeof idade)
 
+// Atribuir um valor à variável
 idade = 33
 
+// Imprimir dados novamente
 console.log("Valor da idade:", idade)
 console.log("Tipo da idade:", typeof idade)
 
+// Atribuir o valor "nulo" à variável
 idade = null
 
+// Imprimir dados novamente 
 console.log("Valor da idade:", idade)
 console.log("Tipo da idade:", typeof idade)
