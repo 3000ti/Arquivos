beb5767602a6108e9707b0fb7229e1bb877b9052
3000ti <166620549+3000ti@users.noreply.github.com>
2024-04-16 20:12:59 -0300

    [update] Comentários no exemplo 7

--- aula-05/exemplo-7-tipos/index.js
+++ aula-05/exemplo-7-tipos/index.js
@@ -1,9 +1,11 @@
 
-const nome = "José"
-const sobrenome = "Silveira"
-const idade = 88
-const estudante = false
+// Criar diferentes tipos de variáveis
+const nome = "José" // string
+const sobrenome = "Silveira" // string
+const idade = 88 // number
+const estudante = false // boolean
 
+// Imprimir os valores no console
 console.log("Nome:", nome)
 console.log("Sobrenome:", sobrenome)
 console.log("Idade:", idade)
