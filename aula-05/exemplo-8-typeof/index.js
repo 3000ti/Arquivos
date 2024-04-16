e516c9d3923559800e079bbd4d4acdb79047a39a
3000ti <166620549+3000ti@users.noreply.github.com>
2024-04-16 20:13:33 -0300

    [update] Comentários no exemplo 8

--- aula-05/exemplo-8-typeof/index.js
+++ aula-05/exemplo-8-typeof/index.js
@@ -1,8 +1,10 @@
 
+// Criar diferentes tipos de variáveis
 const nome = "José"
 const idade = 88
 const estudante = false
 
+// Imprimir os tipos das variáveis no console 
 console.log("Tipo do nome:", typeof nome)
 console.log("Tipo da idade:", typeof idade)
 console.log("Tipo do estudante:", typeof estudante)
