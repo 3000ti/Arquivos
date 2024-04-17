d5f7c35b03353522c5754039d0422f37c24f9fa4
3000ti <166620549+3000ti@users.noreply.github.com>
2024-04-17 12:01:00 -0300

    [update] Exercício final

--- aula-07/exemplo-8-ficha/index.js
+++ aula-07/exemplo-8-ficha/index.js
@@ -1,8 +1,27 @@
 
-// Perguntar o nome da pessoa, o ano de nascimento e o ano atual
+// Perguntar o nome da pessoa
+const nome = prompt("Qual é o seu nome?")
 
-// Imprimir a ficha da pessoa:
-// Nome da pessoa
-// Idade da pessoa
-// Booleano de maioridade
-// Quantos anos terá em 2050
+// Perguntar o ano de nascimento e converter para número (prompt sempre devolve tipo string)
+const anoNascimento = Number(prompt("Em que ano você nasceu?"))
+
+// Perguntar o ano atual e converter para número (prompt sempre devolve tipo string)
+const anoAtual = Number(prompt("Em que ano nós estamos?"))
+
+// Calcular a idade atual da pessoa
+const idadeAtual = anoAtual - anoNascimento
+
+// Calcular a idade da pessoa em 2050
+const idadeEm2050 = 2050 - anoNascimento
+
+// Verificar se a pessoa é maior de idade
+const idadeMaioridade = 18
+const pessoaMaiorDeIdade = idadeAtual >= idadeMaioridade
+
+// Imprimir dados no console
+console.log("Nome:", nome)
+console.log("Ano de nascimento:", anoNascimento)
+console.log("Ano atual:", anoAtual)
+console.log("Idade atual:", idadeAtual)
+console.log("Idade em 2050:", idadeEm2050)
+console.log("Maior de idade:", pessoaMaiorDeIdade)
